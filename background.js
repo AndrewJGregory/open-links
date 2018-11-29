96da51d0b6a4c0dd32f6e942964b08a93c93eb33
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-11-29 17:29:55 -0500

    open links in background ALL the time

--- background.js
+++ background.js
@@ -1,3 +1,11 @@
-chrome.runtime.onMessage.addListener(
-  request => request.urls.forEach(url => chrome.tabs.create({ url, active: false }))
+chrome.runtime.onMessage.addListener(request =>
+  request.urls.forEach(url => chrome.tabs.create({ url, active: false })),
 );
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+  if (changeInfo.status === "complete" && tab.title === "Jobberwocky") {
+    chrome.tabs.executeScript(tabId, {
+      file: "content.js",
+    });
+  }
+});
--- content.js
+++ content.js
@@ -1,10 +1,10 @@
 (() => {
-  const containers = [...document.querySelectorAll('small')];
+  const containers = [...document.querySelectorAll("small")];
   const urls = containers.reduce((urls, container) => {
     const { href } = container.children[0];
     const isUnique = urls[0] !== href;
     if (href !== window.location.href && isUnique) urls.push(href);
     return urls;
-  }, [])
+  }, []);
   chrome.runtime.sendMessage({ urls });
 })();
