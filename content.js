a154aa8db360f13bb58f18a1fafcc1781ffc11c5
Andrew Gregory <andrewjgregoryajg@gmail.com>
2018-11-29 14:12:29 -0500

    prevent opening duplicate links if both fields are the same

--- content.js
+++ content.js
@@ -2,7 +2,8 @@
   const containers = [...document.querySelectorAll('small')];
   const urls = containers.reduce((urls, container) => {
     const { href } = container.children[0];
-    if (href !== window.location.href) urls.push(href);
+    const isUnique = urls[0] !== href;
+    if (href !== window.location.href && isUnique) urls.push(href);
     return urls;
   }, [])
   chrome.runtime.sendMessage({ urls });
