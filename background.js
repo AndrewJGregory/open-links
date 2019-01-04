ff17d70c8fc0971e7a7c48e8e20192bada34f08a
Andrew Gregory <andrewjgregoryajg@gmail.com>
2019-01-04 14:23:40 -0500

    open links when navigating directly to project page

--- background.js
+++ background.js
@@ -9,16 +9,30 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
     chrome.tabs.executeScript(tabId, {
       code: `
       (() => {
-      const tabIndex = ${tab.index};
-      const containers = [...document.querySelectorAll("small")];
-      const urls = containers.reduce((urls, container) => {
-        const { href } = container.children[0];
-        const isUnique = urls[0] !== href;
-        if (href !== window.location.href && isUnique) urls.push(href);
-        return urls;
-      }, []);
-      chrome.runtime.sendMessage({ urls, tabIndex });
-    })()`,
+        const sendMessage = containers => {
+          const tabIndex = ${tab.index};
+          const urls = containers.reduce((urls, container) => {
+            const { href } = container.children[0];
+            const isUnique = urls[0] !== href;
+            if (href !== window.location.href && isUnique) urls.push(href);
+            return urls;
+          }, []);
+          chrome.runtime.sendMessage({ urls, tabIndex });
+        }
+        
+        let containers = [...document.querySelectorAll("small")];
+        if (!containers.length) {
+          const id = setInterval(() => {
+            containers = [...document.querySelectorAll("small")];
+            if (containers.length) {
+              clearInterval(id);
+              sendMessage(containers);
+            }
+          }, 200);
+        } else {
+          sendMessage(containers)
+        }
+      })();`
     });
   }
 });
